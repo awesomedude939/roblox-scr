0048da1c9273bae00ee33ea2b7c3564e7b41c2bf
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-02-12 17:17:58 -0300

    Create source-latest.lua

---
+++ adv_spy/source-latest.lua
@@ -0,0 +1,86 @@
+getgenv().ExcludeListRS = {}
+rconsoleclear()
+rconsolename("Adv Spy")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/awesomedude939/roblox-scr/main/adv_spy/getdatatype.lua"),"this is custom stack")()
+ -- sorry for being script kiddie :( 
+ -- i stole some code from https://pastebin.com/raw/BDhSQqUU
+ -- just the function GetFullPathOfAnInstance ok
+function GetFullPathOfAnInstance(instance)
+    local name = instance.Name
+    local head = (#name > 0 and '.' .. name) or "['']"
+    
+    if not instance.Parent and instance ~= game then
+        return head .. " --[[ PARENTED TO NIL OR DESTROYED ]]"
+    end
+    
+    if instance == game then
+        return "game"
+    elseif instance == workspace then
+        return "workspace"
+    else
+        local _success, result = pcall(game.GetService, game, instance.ClassName)
+        
+        if result then
+            head = ':GetService("' .. instance.ClassName .. '")'
+        elseif instance == client then
+            head = '.LocalPlayer' 
+        else
+            local nonAlphaNum = name:gsub('[%w_]', '')
+            local noPunct = nonAlphaNum:gsub('[%s%p]', '')
+            
+            if tonumber(name:sub(1, 1)) or (#nonAlphaNum ~= 0 and #noPunct == 0) then
+                head = '["' .. name:gsub('"', '\\"'):gsub('\\', '\\\\') .. '"]'
+            elseif #nonAlphaNum ~= 0 and #noPunct > 0 then
+                head = '[' .. toUnicode(name) .. ']'
+            end
+        end
+    end
+    
+    return GetFullPathOfAnInstance(instance.Parent) .. head
+end
+
+local OldNamecall
+OldNamecall = hookmetamethod(game, "__namecall", function(...)
+	local args = {...}
+	local Self = args[1]
+	local namecall = getnamecallmethod()
+	local formatx = "game."
+	if namecall == "FireServer" and not table.find(getgenv().ExcludeListRS, Self.Name) then
+	    pcall(function()
+	    formatx = formatx..GetFullPathOfAnInstance(Self)
+	    formatx = formatx..":FireServer("
+	    local s,e = pcall(function()
+	    for i,v in pairs(args) do 
+	        if i ~= 1 and i ~= #args then 
+	        formatx = formatx..getdatatype(typeof(v),v)..", "
+	        elseif i == #args then
+	            formatx = formatx..getdatatype(typeof(v),v)
+	        end
+	    end
+	    end)
+	    formatx = formatx..") \n"
+	    s,e = pcall(function()
+	    rconsoleprint(tostring(formatx))
+	    end)
+	    end)
+	elseif namecall == "InvokeServer" and not table.find(getgenv().ExcludeListRS, Self.Name) then    
+	    pcall(function()
+	    formatx = formatx..GetFullPathOfAnInstance(Self)
+	    formatx = formatx..":InvokeServer("
+	    local s,e = pcall(function()
+	    for i,v in pairs(args) do 
+	        if i ~= 1 and i ~= #args then 
+	        formatx = formatx..getdatatype(typeof(v),v)..", "
+	        elseif i == #args then
+	            formatx = formatx..getdatatype(typeof(v),v)
+	        end
+	    end
+	    end)
+	    formatx = formatx..") \n"
+	    s,e = pcall(function()
+	    rconsoleprint(tostring(formatx))
+	    end)
+	    end)
+	end
+	return OldNamecall(...)
+end)
