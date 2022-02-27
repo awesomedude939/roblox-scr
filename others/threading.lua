fe5f04c0397711814d00dfa11796249b7d740174
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-02-27 18:08:15 -0300

    Update threading.lua

--- others/threading.lua
+++ others/threading.lua
@@ -1,1 +1,7 @@
+--[[
+Thread | Threading.new([function] Function,[table] Args)
+Thread.start()
+
+]]
+
 local a={}function a.new(b,c)if type(c)~="table"then error("Table expected, got "..type(c))end;if type(b)~="function"then error("Function expected, got "..type(c))end;local d={}function d.start()spawn(function()b(table.unpack(c))end)end;return d end
