d50997f3dbb361b621a03af802a4e90f71f86bb6
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-02-27 18:10:08 -0300

    Update threading.lua

--- others/threading.lua
+++ others/threading.lua
@@ -4,4 +4,4 @@ Thread.start()
 
 ]]
 
-local a={}function a.new(b,c)if type(c)~="table"then error("Table expected, got "..type(c))end;if type(b)~="function"then error("Function expected, got "..type(c))end;local d={}function d.start()spawn(function()b(table.unpack(c))end)end;return d end
+local a={}function a.new(b,c)if type(c)~="table" and type(c)~="nil"then error("Table expected, got "..type(c))end;if type(b)~="function"then error("Function expected, got "..type(c))end;local d={}function d.start()spawn(function()b(table.unpack(c))end)end;return d end
