534f25c655caafae855c0d216c786b27edf4c485
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-02-27 18:04:49 -0300

    Update threading.lua

--- others/threading.lua
+++ others/threading.lua
@@ -1,6 +1,12 @@
 local threading = {}
 
 function threading.new(func,args)
+	if type(args) ~= "table" then 
+		error("Table expected, got "..type(args))
+	end
+	if type(func) ~= "function" then 
+		error("Function expected, got ".. type(args))
+	end
 	local object = {}
 	function object.start()
 		spawn(function()
