7467e6a79a7a62313a3c7938b8a709723e333145
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-04-18 22:27:59 -0300

    Update invertcolor.lua

--- others/invertcolor.lua
+++ others/invertcolor.lua
@@ -1,3 +1,7 @@
 return function(color3)
-    return Color3.fromRGB(255-color3.R,255-color3.G,255-color3.B)
+    if color3.R < 1 then
+        return Color3.new(1-color3.R,1-color3.G,1-color3.B)
+    else
+        return Color.fromRGB(255-color3.R,255-color3.G,255-color3.B)
+    end
 end
