b1cb642dd20dbffc1d5c93ddbe3fcc73afc5f4d2
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-10-16 17:58:00 -0300

    Create source.lua

---
+++ RSTRS/source.lua
@@ -0,0 +1,2 @@
+rconsolename("RSTRS")
+rconsoleprint("\27[0;37;40mRoblox Studio to Retro Studio v1.0\nMade by : awesomedude939\n\27[0;37;41m[!] DO NOT PAY FOR ROBLOX EXPLOIT SUBSCRIPTIONS [!]\27[0;37;40m")
